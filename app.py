52a631706145a9921f5e9bb94023b1829ca952f7
Edgar Tuñon <edgartu@outlook.com>
2025-04-14 09:00:44 -0300

    Se creo la ruta con Flask en el archivo app

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+
+app.secret_key = 'CLAVE_SECRETA_0011'
+
+@app.route('/')
+def inicio():
+    return render_template('index.html', nav_title="inicio")
+
+@app.route('/contactame')
+def contact():
+    return render_template('contactame.html', nav_title="contacto")
+
+
+@app.route('/proyetos')
+def proyectos():
+    return render_template('proyectos.html', nav_title="proyectos")
+
+
+@app.route('/lenguajes')
+def lenguajes():
+    return render_template('lenguajes.html', nav_title="lenguajes")
+
+
+@app.route('/AcercaDe')
+def acerca_de():
+    return render_template('Acerca_de.html', nav_title="acerca de")
+
+
+if __name__=='__main__':
+    app.run(debug=True)
